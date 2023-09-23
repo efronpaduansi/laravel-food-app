d1ecc9a2dc82933fb5dbf1948ee851d0f5ee7a01
efronpaduansi <eufrondpaduansi@gmail.com>
2023-09-23 12:57:10 +0700

    Change title

--- resources/views/web/welcome.blade.php
+++ resources/views/web/welcome.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.web.app')
 
 @section('title')
-    Dapure Masagena
+    Dapoer Masagena
 @endsection
 
 <!-- ======= Hero Section ======= -->
@@ -9,37 +9,38 @@
 <!-- End Hero Section -->
 
 @section('content')
-  <main id="main">
-    @include('layouts.web.partials.navbar')
-    <!-- ======= Menu Section ======= -->
-    <section id="menu" class="menu">
-      <div class="container" data-aos="fade-up">
-        <div class="section-header">
-          <h1>Our Menu</h1>
-          <h2>Pilih menu kesukaanmu di sini!</h2>
-        </div>
-        <div class="tab-content" data-aos="fade-up" data-aos-delay="300">
-          <div class="tab-pane fade active show" id="menu-starters">
-            <div class="row gy-5">
-              @foreach ($menus as $menu)
-              <div class="col-lg-4 menu-item">
-                <a href="assets/img/menu/menu-item-1.png" class="glightbox"><img src="{{ asset('images/menu/'. $menu->thumbnail) }}" class="menu-img img-fluid" alt=""></a>
-                <h4>{{ $menu->name }}</h4>
-                <p class="price">
-                  {{ "Rp." . number_format($menu->price, 2) }}
-                </p>
-                <a href="{{ route('guest.book.new', $menu->id) }}" class="btn btn-success">Pesan</a>
-              </div>
-              @endforeach
+    <main id="main">
+        @include('layouts.web.partials.navbar')
+        <!-- ======= Menu Section ======= -->
+        <section id="menu" class="menu">
+            <div class="container" data-aos="fade-up">
+                <div class="section-header">
+                    <h1>Our Menu</h1>
+                    <h2>Pilih menu kesukaanmu di sini!</h2>
+                </div>
+                <div class="tab-content" data-aos="fade-up" data-aos-delay="300">
+                    <div class="tab-pane fade active show" id="menu-starters">
+                        <div class="row gy-5">
+                            @foreach ($menus as $menu)
+                                <div class="col-lg-4 menu-item">
+                                    <a href="assets/img/menu/menu-item-1.png" class="glightbox"><img
+                                            src="{{ asset('images/menu/' . $menu->thumbnail) }}" class="menu-img img-fluid"
+                                            alt=""></a>
+                                    <h4>{{ $menu->name }}</h4>
+                                    <p class="price">
+                                        {{ 'Rp.' . number_format($menu->price, 2) }}
+                                    </p>
+                                    <a href="{{ route('guest.book.new', $menu->id) }}" class="btn btn-success">Pesan</a>
+                                </div>
+                            @endforeach
+                        </div>
+                    </div>
+                    <!-- End Starter Menu Content -->
+                </div>
             </div>
-          </div>
-          <!-- End Starter Menu Content -->
-        </div>
-      </div>
-    </section>
-  </main>
-   <!-- ======= Footer ======= -->
-   @include('layouts.web.partials.footer')
-   <!-- End Footer -->
+        </section>
+    </main>
+    <!-- ======= Footer ======= -->
+    @include('layouts.web.partials.footer')
+    <!-- End Footer -->
 @endsection
-
